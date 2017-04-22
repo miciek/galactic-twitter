cefbcb2ac97ef7d592a57d5c541dcc52c49727ac
miciek <miciek@gmail.com>
2017-04-22 22:08:26 +0200

    Add first draft of the fifth problem: convoluted logic solved by foldLeft

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ This app serves as a simple practical example of strongly-typed functional appro
 ### Problem #2: Not handling failures 
 ### Problem #3: Handling lists of algebraic data types
 ### Problem #4: Cryptic return types
+### Problem #5: Convoluted logic using IFs  
--- src/main/scala/com/michalplachta/galactic/service/Tweets.scala
+++ src/main/scala/com/michalplachta/galactic/service/Tweets.scala
@@ -16,7 +16,7 @@ object Tweets {
     val futureTweets: Future[List[Tweet]] = for {
       citizen ← DbClient.findCitizenByName(citizenName)
       tweets ← DbClient.getTweetsFor(citizen)
-    } yield censorTweetsFor(tweets, citizen)
+    } yield censorTweetsUsingFilters(tweets, citizen)
     futureTweets.onComplete { result ⇒
       val value: RemoteData[List[Tweet]] =
         result match {
@@ -28,24 +28,57 @@ object Tweets {
     cachedTweetsFor.getOrElse(citizenName, NotRequestedYet())
   }
 
-  def censorTweetsFor(tweets: List[Tweet], citizen: Citizen): List[Tweet] = {
+  // PROBLEM #5: Convoluted logic using IFs
+  def censorTweets(tweets: List[Tweet], citizen: Citizen): List[Tweet] = {
     tweets.map { t ⇒
-      if (isProDarkSide(t))
-        addMoreDarkSide(t)
-      else if (isProLightSide(t))
-        replaceForceWithDarkSide(t)
-      else if (isGeneralWisdom(t))
-        addMoreDarkSide(replaceDarkSideWithForce(t))
-      else if (isProRebellion(t.author) && !isJoke(t))
-        hailEmpire(t)
-      else if (isProEmpire(t.author) && isJoke(t))
-        trashRebellion(t)
-      else if (!isProRebellion(t.author) && !isProEmpire(t.author))
-        makeJoke(addEvenMoreForce(addMoreForce(t)))
-      else if (isProEmpire(t.author))
-        addMoreDarkSide(t)
-      else
-        sacrificeForEmpire(t)
+      var tweet = t
+      tweet = if (isProDarkSide(tweet)) addMoreDarkSide(tweet) else tweet
+      tweet = if (isProLightSide(tweet)) replaceForceWithDarkSide(tweet) else tweet
+      tweet = if (isGeneralWisdom(tweet)) addMoreDarkSide(replaceForceWithDarkSide(tweet)) else tweet
+      tweet = if (isProRebellion(tweet) && !isJoke(tweet)) hailEmpire(tweet) else tweet
+      tweet = if (isProEmpire(tweet) && isJoke(tweet)) trashRebellion(tweet) else tweet
+      tweet = if (!isProRebellion(tweet) && !isProEmpire(tweet)) makeJoke(addEvenMoreForce(addMoreForce(tweet))) else tweet
+      tweet = if (isProEmpire(tweet)) addMoreDarkSide(tweet) else tweet
+      sacrificeForEmpire(tweet)
+    }
+  }
+
+  case class CensorFilter(shouldManipulate: Tweet ⇒ Boolean, manipulate: Tweet ⇒ Tweet)
+
+  implicit class PredicateWithLogic[A](p1: A ⇒ Boolean) {
+    def and(p2: A ⇒ Boolean): A ⇒ Boolean = {
+      a ⇒ p1(a) && p2(a)
+    }
+
+    def andNot(p2: A ⇒ Boolean): A ⇒ Boolean = {
+      a ⇒ p1(a) && !p2(a)
+    }
+  }
+
+  def not[A](p: A ⇒ Boolean): A ⇒ Boolean = a ⇒ !p(a)
+
+  def always[A]: A ⇒ Boolean = _ ⇒ true
+
+  val censorFilters: List[CensorFilter] = List(
+    CensorFilter(isProDarkSide, addMoreDarkSide),
+    CensorFilter(isProLightSide, replaceForceWithDarkSide),
+    CensorFilter(isGeneralWisdom, (replaceForceWithDarkSide _).andThen(addMoreDarkSide)),
+    CensorFilter((isProRebellion _).andNot(isJoke), hailEmpire),
+    CensorFilter((isProEmpire _).and(isJoke), trashRebellion),
+    CensorFilter(not(isProRebellion).andNot(isProEmpire), (addMoreForce _).andThen(addEvenMoreForce).andThen(makeJoke)),
+    CensorFilter(isProEmpire, addMoreDarkSide),
+    CensorFilter(always, sacrificeForEmpire)
+  )
+
+  // SOLUTION #5: Logic as data, fold data to run the logic
+  def censorTweetsUsingFilters(tweets: List[Tweet], citizen: Citizen): List[Tweet] = {
+    tweets.map { tweet ⇒
+      censorFilters.foldLeft(tweet) { (tweet, filter) ⇒
+        if (filter.shouldManipulate(tweet))
+          filter.manipulate(tweet)
+        else
+          tweet
+      }
     }
   }
 
@@ -53,16 +86,16 @@ object Tweets {
 
   private def isProDarkSide(tweet: Tweet): Boolean = tweet.text.split("the Force").length < tweet.text.split("DarkSide").length
 
-  private def isProEmpire(citizen: Citizen): Boolean = {
-    citizen match {
+  private def isProEmpire(tweet: Tweet): Boolean = {
+    tweet.author match {
       case Sith(_)            ⇒ true
       case Stormtrooper(_, _) ⇒ true
       case _                  ⇒ false
     }
   }
 
-  private def isProRebellion(citizen: Citizen): Boolean = {
-    citizen match {
+  private def isProRebellion(tweet: Tweet): Boolean = {
+    tweet.author match {
       case Jedi(_)  ⇒ true
       case Rebel(_) ⇒ true
       case _        ⇒ false
@@ -79,6 +112,5 @@ object Tweets {
   private def makeJoke(tweet: Tweet) = Tweet(tweet.text + " And I'm not really interested in your opinion, 3PO.", tweet.author)
   private def addMoreForce(tweet: Tweet) = Tweet("May the Force be with you! " + tweet.text, tweet.author)
   private def addEvenMoreForce(tweet: Tweet) = Tweet(tweet.text + " I'm one with the Force, the Force is with me.", tweet.author)
-  private def replaceDarkSideWithForce(tweet: Tweet) = Tweet(tweet.text.replaceAll("Dark Side", "Force"), tweet.author)
   private def replaceForceWithDarkSide(tweet: Tweet) = Tweet(tweet.text.replaceAll("Force", "Dark Side"), tweet.author)
 }
