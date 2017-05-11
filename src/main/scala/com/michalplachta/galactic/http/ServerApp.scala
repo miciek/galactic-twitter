cc8fe82dfb19be0fa9f11a5d0521a0b5bc89c727
miciek <miciek@gmail.com>
2017-05-11 07:54:10 +0100

    Make the type in ServerApp more explicit

--- src/main/scala/com/michalplachta/galactic/http/ServerApp.scala
+++ src/main/scala/com/michalplachta/galactic/http/ServerApp.scala
@@ -3,6 +3,7 @@ package com.michalplachta.galactic.http
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.server.{ HttpApp, Route }
 import com.michalplachta.galactic.service.{ FollowersService, TweetsService }
+import com.michalplachta.galactic.values.{ RemoteData, Tweet }
 import spray.json._
 
 object ServerApp extends App with RemoteDataJsonSupport with TweetJsonSupport with SprayJsonSupport {
@@ -10,12 +11,18 @@ object ServerApp extends App with RemoteDataJsonSupport with TweetJsonSupport wi
     def route: Route =
       path("followers" / Segment) { citizenName ⇒
         get {
-          complete(FollowersService.getFollowers(citizenName).toJson)
+          complete {
+            val remoteFollowers: RemoteData[Int] = FollowersService.getFollowers(citizenName)
+            remoteFollowers
+          }
         }
       } ~
         path("tweets" / Segment) { citizenName ⇒
           get {
-            complete(TweetsService.getTweetsFor(citizenName).toJson)
+            complete {
+              val remoteTweets: RemoteData[List[Tweet]] = TweetsService.getTweetsFor(citizenName)
+              remoteTweets
+            }
           }
         }
   }
